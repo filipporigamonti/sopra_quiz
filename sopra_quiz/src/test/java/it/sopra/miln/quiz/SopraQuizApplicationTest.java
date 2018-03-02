cc683cb9bef3f1297ead3563d72d9409abb8233d
RIGAMONTI Filippo <filippo.rigamonti@soprasteria.com>
2018-03-02 11:29:31 +0100

    Aggiunta del PATH per i test

---
+++ sopra_quiz/src/test/java/it/sopra/miln/quiz/SopraQuizApplicationTest.java
@@ -0,0 +1,13 @@
+package it.sopra.miln.quiz;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SopraQuizApplicationTest {
+
+}
